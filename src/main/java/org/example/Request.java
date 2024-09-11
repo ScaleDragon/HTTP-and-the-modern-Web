94b38fb6c00aa584496e61b537066efeed41d4e7
Vyacheslav L. <Dwek18@mail.ru>
2024-09-11 22:49:33 +0300

    add task x-www-form-urlencoded

---
+++ src/main/java/org/example/Request.java
@@ -0,0 +1,101 @@
+package org.example;
+
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URLEncodedUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Request {
+    private final String method;
+    private final String path;
+    private final BufferedReader body;
+    private final List<NameValuePair> queryParams;
+    private List<NameValuePair> postParams;
+    private Map<String, String> headers;
+
+    public Request(String method, String path, InputStream body) {
+        this.method = method;
+        this.body = new BufferedReader(new InputStreamReader(body));
+        String[] parts = path.split("\\?");
+        this.path = parts[0];
+        if (parts.length > 1) {
+            this.queryParams = URLEncodedUtils.parse(parts[1], StandardCharsets.UTF_8);
+        } else {
+            this.queryParams = List.of(); // Создаем пустой список, если нет параметров
+        }
+    }
+
+    //    public String getPostParam(String name) {
+//        return postParams.stream()
+//                .filter(p -> p.getName().equalsIgnoreCase(name))
+//                .map(NameValuePair::getValue)
+//                .findFirst()
+//                .orElse(null);
+//    }
+    public List<NameValuePair> getPostParam(String name) {
+        return this.postParams.stream()
+                .filter(p -> p.getName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+    }
+
+    public List<NameValuePair> getPostParam() {
+        return Collections.unmodifiableList(this.postParams);
+    }
+
+    public List<NameValuePair> getPostParams() {
+        return postParams;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public BufferedReader getBody() {
+        return body;
+    }
+
+
+    public List<NameValuePair> getQueryParam(String name) {
+        return queryParams.stream().filter(p -> p.getName().equals(name)).collect(Collectors.toList());
+
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    private void parseBody() throws IOException {
+        if ("POST".equalsIgnoreCase(method)) {
+            String contentType = getHeader("Content-Type");
+            if (contentType != null && contentType.contains("application/x-www-form-urlencoded")) {
+                StringBuilder bodyBuilder = new StringBuilder();
+                String line;
+                while ((line = body.readLine()) != null) {
+                    bodyBuilder.append(line);
+                }
+                String body = bodyBuilder.toString();
+                this.postParams = URLEncodedUtils.parse(body, StandardCharsets.UTF_8);
+            } else {
+                this.postParams = Collections.emptyList();
+            }
+        } else {
+            this.postParams = Collections.emptyList();
+        }
+    }
+
+    private String getHeader(String headerName) {
+        return headers.getOrDefault(headerName.toLowerCase(), null);
+    }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,103 @@
+package org.example;
+
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URLEncodedUtils;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static ExecutorService pool;
+    private static final ConcurrentHashMap<String, ConcurrentHashMap<String, Handler>> handlers = new ConcurrentHashMap<>();
+
+    public Server(int poolSize) {
+        pool = Executors.newFixedThreadPool(poolSize);
+    }
+
+    public void startingServer(int port) {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final Socket socket = serverSocket.accept();
+                pool.submit(() -> {
+                    try {
+                        requestProcessing(socket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();  // Лучше использовать logging
+                        }
+                    }
+                });
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        } finally {
+            pool.shutdown();
+        }
+    }
+
+    private static void requestProcessing(Socket socket) throws IOException {
+        while (true) {
+            try (final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())
+            ) {
+                final String requestLine = in.readLine();
+                if (requestLine == null) {
+                    throw new IOException("Invalid request line");
+                }
+                final String[] parts = requestLine.split(" ");
+                if (parts.length != 3) {
+                    Server.badRequest(out);
+                    return;
+                }
+                final String method = parts[0];
+                final String path = parts[1];
+
+                InputStream inputStreamBody = socket.getInputStream();
+                Request request = new Request(method, path, inputStreamBody);
+
+                Handler handler = handlers.getOrDefault(method, new ConcurrentHashMap<>()).get(request.getPath());
+                if (handler == null) {
+                    notFound(out);
+                } else {
+                    handler.handle(request, out);
+                    out.flush();
+                }
+                System.out.println(request.getQueryParams());
+                System.out.println(request.getPostParam());
+            }
+        }
+    }
+
+    protected void addHandler(String method, String path, Handler handler) {
+        if (!handlers.containsKey(method)) {
+            handlers.put(method, new ConcurrentHashMap<>());
+        }
+        handlers.get(method).put(path, handler);
+    }
+
+    private static void badRequest(BufferedOutputStream out) throws IOException {
+        String response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+        out.write(response.getBytes());
+    }
+
+    private static void notFound(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 " + "404" + " " + "Not found" + "\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+}
